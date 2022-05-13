4720aecafd8c92f3fef274dd4bbfab42b1bf9e20
Strugoeden121 <edensf10@gmail.com>
2022-05-13 18:27:28 +0300

    feature: adding Card.cpp after compilation (card_b)

--- Card.cpp
+++ Card.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include "utilities.h"
 #include "Player.h"
-#include "utilities.h"
+#include "Card.h"
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -17,7 +17,78 @@ Card::Card(CardType type, const CardStats& stats){
   
 }
 
-void applyEncounter(Player& player) const{
+static void battleEncounter(Player& player, const CardStats& stats) {
+//Plays an encounter of the player with a battle card.
+
+  assert(player && stats);
+  bool isWonBattle = player.getAttackStrength() >= stats.force;
+  printBattleResult(isWonBattle);
+
+  if(isWonBattle){
+      player.levelUp();
+      player.addCoins(stats.loot);
+  }
+
+  else{
+      player.damage(stats.hpLossOnDefeat);
+  }
+}
+
+static void buffEncounter(Player& player, const CardStats& stats) {
+//Plays an encounter of the player with a buff card.
+
+  assert(player && stats);
+  bool isEnoughMoney = player.pay(stats.cost);
+  if(isEnoughMoney){
+      player.buff(stats.buff);
+  }
+}
+
+static void healEncounter(Player& player, const CardStats& stats) {
+  //Plays an encounter of the player with a heal card.
+
+    assert(player && stats);
+    bool isEnoughMoney = player.pay(stats.cost);
+
+    if(isEnoughMoney){
+        player.heal(stats.heal);
+    }
+}
+
+static void treasureEncounter(Player& player, const CardStats& stats) {
+  //Plays an encounter of the player with a treasure card.
+
+    assert(player && stats);
+    player.addCoins(stats.cost);
+}
+
+void Card::printInfo() const{
+//Plays an encounter of the player with a random card.
+  
+  assert(player);
+  switch(this->m_effect){
+  
+    case CardType::Battle:
+      printBattleCardInfo(this->m_stats);
+      break;
+
+       
+    case CardType::Buff:
+      printBuffCardInfo(this->m_stats);
+      break;
+      
+    case CardType::Heal:
+      printHealCardInfo(this->m_stats);
+      break;
+      
+    case CardType::Treasure:
+      printTreasureCardInfo(this->m_stats);
+      break;
+
+  }
+}
+
+void Card::applyEncounter(Player& player) const{
 //Plays an encounter of the player with a random card.
   
   assert(player);
@@ -36,80 +107,10 @@ void applyEncounter(Player& player) const{
       healEncounter(player, this->m_stats);
       break;
       
-    case CardType::Tresure:
-      tresureEncounter(player, this->m_stats);
+    case CardType::Treasure:
+      treasureEncounter(player, this->m_stats);
       break;
 
   }
 }
 
-static void battleEncounter(Player& player, const CardStats& stats) const{
-//Plays an encounter of the player with a battle card.
-
-  assert(player && stats);
-  bool isWonBattle = player.force >= stats.force;
-  utilities::printBattleResult(isWonBattle);
-
-  if(isWonBattle){
-      player.levelup();
-      player.addcoins(stats.loot);
-  }
-
-  else{
-      player.damage(stats.hpLoss);
-  }
-}
-
-static void buffEncounter(Player& player, const CardStats& stats) const{
-//Plays an encounter of the player with a buff card.
-
-  assert(player && stats);
-  bool isEnoughMoney = player.pay(stats.cost);
-  if(isEnoughMoney){
-      player.buff(stats.buff);
-  }
-}
-
-static void buffEncounter(Player& player, const CardStats& stats) const{
-  //Plays an encounter of the player with a heal card.
-
-    assert(player && stats);
-    bool isEnoughMoney = player.pay(stats.cost);
-
-    if(isEnoughMoney){
-        player.heal(stats.heal);
-    }
-}
-
-static void tresureEncounter(Player& player, const CardStats& stats) const{
-  //Plays an encounter of the player with a tresure card.
-
-    assert(player && stats);
-    player.addcoins(stats.coins);
-}
-
-void printInfo() const{
-//Plays an encounter of the player with a random card.
-  
-  assert(player);
-  switch(this->m_effect){
-  
-    case CardType::Battle:
-      utilities::printBattleCardInfo(stats);
-      break;
-
-       
-    case CardType::Buff:
-      utilities::printBuffCardInfo(stats);
-      break;
-      
-    case CardType::Heal:
-      utilities::printHealCardInfo(stats);
-      break;
-      
-    case CardType::Tresure:
-      utilities::printTreasureCardInfo(stats);
-      break;
-
-  }
-}
